c60003d235efd5f1ace3d5a36b47287c9af37adb
emarcha466 <145332884+emarcha466@users.noreply.github.com>
2023-12-14 04:27:08 +0100

    estilos realizar solicitud, valia tutor

--- javaScript/realizarSolicitudJS.js
+++ javaScript/realizarSolicitudJS.js
@@ -6,38 +6,48 @@ window.addEventListener('load', function () {
 
 
     //rellenar los cursos con los destinatarios de la convocatoria
-    this.fetch("./api/ConvocatoriaDestinatarioApi.php?idConvocatoria="+encodeURIComponent(idConvo),{
+    this.fetch("./api/ConvocatoriaDestinatarioApi.php?idConvocatoria=" + encodeURIComponent(idConvo), {
         method: 'GET'
     })
-    .then(x=>x.json())
-    .then(dest =>{
-        if(dest){
-            dest.forEach(element => {
-                let option = this.document.createElement("option")
-                let select = this.document.getElementById("curso")
-                option.value = element.codigoGrupo
-                option.innerHTML= element.codigoGrupo
-                select.appendChild(option)
-            });
-            
-        }
-    })
+        .then(x => x.json())
+        .then(dest => {
+            if (dest) {
+                dest.forEach(element => {
+                    let option = this.document.createElement("option")
+                    let select = this.document.getElementById("curso")
+                    option.value = element.codigoGrupo
+                    option.innerHTML = element.codigoGrupo
+                    select.appendChild(option)
+                });
 
+            }
+        })
+
+    //muestra los datos de tutor si es menor
     fechaNac.addEventListener('change', function () {
         var fechaNacimiento = new Date(this.value);
+        let inputsTutor = Array.from(document.getElementById("infoTutor").children);
+
+        //si es menor de edad los datos de tutor tambien son requeridos
         if (esMayorDeEdad(fechaNacimiento)) {
             document.getElementById('infoTutor').style.display = 'none';
+            inputsTutor.forEach(input => {
+                input.removeAttribute('data-valida');
+            })
         } else {
             document.getElementById('infoTutor').style.display = '';
+            inputsTutor.forEach(input => {
+                input.setAttribute('data-valida', 'relleno');
+            })
         }
     })
 
     btnSolicitud.addEventListener('click', function (ev) {
         ev.preventDefault()
-        if(this.form.valida()){
+        if (this.form.valida()) {
             let formData = new FormData(formSolicitud);
             formData.append("idConvocatoria", idConvo)
-            
+
             //llamada a la api para crear la solicitud
             fetch("./api/SolicitudApi.php", {
                 method: 'POST',
@@ -48,7 +58,7 @@ window.addEventListener('load', function () {
                     if (respuesta.status == "success") {
                         console.log(respuesta.message + " " + respuesta.id);
                         //uso encodeURIComponent para evitar que pueda cambiar el valor al pasarlo por url
-    
+
                         //si se ha creado la solicitud correctamente, añado los itemBaremables a la baremacion
                         fetch("./api/BaremacionApi.php?idConvocatoria=" + encodeURIComponent(idConvo) + "&idSolicitud=" + encodeURIComponent(respuesta.id), {
                             method: 'POST'
@@ -57,48 +67,48 @@ window.addEventListener('load', function () {
                             .then(response => {
                                 //si se ha creado correctamente, mando los datos de consulta de la solicitud al correo proporcionado
                                 if (response.status == "success") {
-    
+
                                     //relleno la plantilla del correo
                                     fetch('./views/plantillasCorreo/datosSolicitud.html')
                                         .then(x => x.text())
                                         .then(plantilla => {
                                             let html = plantilla.replace('{dni}', formData.get("dni"))
-                                                    .replace('{idSolicitud}', respuesta.id)
-                                                    .replace('{pass}', formData.get("pass"));
-    
+                                                .replace('{idSolicitud}', respuesta.id)
+                                                .replace('{pass}', formData.get("pass"));
+
                                             let datos = new FormData();
                                             datos.append('cuerpo', html);
                                             datos.append('destinatario', formData.get("correo"))
                                             datos.append('asunto', "Localizador  de solicitud")
-    
+
                                             fetch('./api/CorreoApi.php', {
                                                 method: 'POST',
                                                 body: datos
                                             })
-                                            .then(x => x.json())
-                                            .then(respC => {
-                                                if(respC.status =="success"){
-                                                    formSolicitud.reset()
-                                                    muestraCorrecto("Solicitud enviada correctamente")
-                                                    //borro el id de la convocatoria del localstorage
-                                                    localStorage.removeItem("idConvo")
-                                                    setTimeout(function(){
-                                                        location.href="?menu=estadoSolicitudLogin"
-                                                    },3000)
-                                                }
-                                            })
+                                                .then(x => x.json())
+                                                .then(respC => {
+                                                    if (respC.status == "success") {
+                                                        formSolicitud.reset()
+                                                        muestraCorrecto("Solicitud enviada correctamente")
+                                                        //borro el id de la convocatoria del localstorage
+                                                        localStorage.removeItem("idConvo")
+                                                        setTimeout(function () {
+                                                            location.href = "?menu=estadoSolicitudLogin"
+                                                        }, 3000)
+                                                    }
+                                                })
                                         });
                                 }
                             })
-    
+
                     } else {
                         muestraError(respuesta.message)
                     }
                 })
-        }else{
+        } else {
             muestraError("Revise los campos del formulario")
         }
-        
+
     })
 
     /**
--- views/realizarSolicitud.php
+++ views/realizarSolicitud.php
@@ -5,42 +5,43 @@
     <p class="error"></p>
     <p class="success"></p>
     <form action="" method="post" id="formSolicitud">
-        <label for="dni">DNI:</label><br>
-        <input type="text" id="dni" name="dni" data-valida="dni"><br>
-        <label for="apellidos">Apellidos:</label><br>
-        <input type="text" id="apellidos" name="apellidos" data-valida="relleno"><br>
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre" data-valida="relleno"><br>
-        <label for="fechaNac">Fecha de Nacimiento:</label><br>
-        <input type="date" id="fechaNac" name="fechaNac" data-valida="fecha"><br>
-        <label for="curso">Curso:</label><br>
+        <label for="dni">DNI:</label>
+        <input type="text" id="dni" name="dni" data-valida="dni">
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" id="apellidos" name="apellidos" data-valida="relleno">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" data-valida="relleno">
+        <label for="fechaNac">Fecha de Nacimiento:</label>
+        <input type="date" id="fechaNac" name="fechaNac" data-valida="fecha">
+        <label for="curso">Curso:</label>
         <select name="curso" id="curso" data-valida="select">
             <option value="-1">Seleccione su curso</option>
-        </select><br>
-        <label for="telefono">Teléfono:</label><br>
-        <input type="tel" id="telefono" name="telefono" data-valida="telefono"><br>
-        <label for="correo">Correo:</label><br>
-        <input type="email" id="correo" name="correo" data-valida="email"><br>
-        <label for="domicilio">Domicilio:</label><br>
-        <input type="text" id="domicilio" name="domicilio" data-valida="relleno"><br>
-        <label for="pass">Contraseña:</label><br>
-        <input type="password" id="pass" name="pass" data-valida="relleno"><br>
-        <label for="imagen">Imagen:</label><br>
-        <button id="openWebcam" onclick="modalFoto(event)">Abrir Webcam</button><br>
+        </select>
+        <label for="telefono">Teléfono:</label>
+        <input type="tel" id="telefono" name="telefono" data-valida="telefono">
+        <label for="correo">Correo:</label>
+        <input type="email" id="correo" name="correo" data-valida="email">
+        <label for="domicilio">Domicilio:</label>
+        <input type="text" id="domicilio" name="domicilio" data-valida="relleno">
+        <label for="pass">Contraseña:</label>
+        <input type="password" id="pass" name="pass" data-valida="relleno">
+        <label for="imagen">Imagen:</label>
+        <button id="openWebcam" onclick="modalFoto(event)" class="btnRealizarSolicitud">Abrir Webcam</button>
         <img id="imgFotoPerfil" src="" alt="Foto Perfil">
-        <input type="text" id="blob" name="imagen" readonly> <br>
-        <div id="infoTutor" style="display: none;">
-            <label for="dniTutor">DNI Tutor:</label><br>
-            <input type="text" id="dniTutor" name="dniTutor"><br>
-            <label for="apellidosTutor">Apellidos Tutor:</label><br>
-            <input type="text" id="apellidosTutor" name="apellidosTutor"><br>
-            <label for="nombreTutor">Nombre Tutor:</label><br>
-            <input type="text" id="nombreTutor" name="nombreTutor"><br>
-            <label for="telefonoTutor">Teléfono Tutor:</label><br>
-            <input type="tel" id="telefonoTutor" name="telefonoTutor"><br>
-            <label for="domicilioTutor">Domicilio Tutor:</label><br>
-            <input type="text" id="domicilioTutor" name="domicilioTutor"><br>
-        </div id="infoTutor">
+        <input type="hidden" id="blob" name="imagen" readonly>
+        <fieldset id="infoTutor" style="display: none;">
+            <legend>Tutor legal</legend>
+            <label for="dniTutor">DNI Tutor:</label>
+            <input type="text" id="dniTutor" name="dniTutor">
+            <label for="apellidosTutor">Apellidos Tutor:</label>
+            <input type="text" id="apellidosTutor" name="apellidosTutor">
+            <label for="nombreTutor">Nombre Tutor:</label>
+            <input type="text" id="nombreTutor" name="nombreTutor">
+            <label for="telefonoTutor">Teléfono Tutor:</label>
+            <input type="tel" id="telefonoTutor" name="telefonoTutor">
+            <label for="domicilioTutor">Domicilio Tutor:</label>
+            <input type="text" id="domicilioTutor" name="domicilioTutor">
+        </fieldset>
         <input type="button" value="Realizar solicitud" id="btnSolicitud" class="btnPantalla">
     </form>
 
